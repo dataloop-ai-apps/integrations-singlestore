bfc7cf2f133a19f6e08dced6b8c2f383075d38fb
ludoHorv <ludo.h@dataloop.ai>
2024-11-18 13:51:40 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# SingleStore Integration
+
+This repository provides a service that enables seamless interaction between **SingleStore** and **Dataloop** using **user-password authentification**. The integration is designed to streamline data processing, table updates, and data uploads between SingleStore and Dataloop datasets.
+
+## Features
+
+- **Secure Authentication** with **user-password authentication** for SingleStore access.
+- **SQL Query Execution** on SingleStore directly from Dataloop using the integrated service.
+- **Dynamic Table Creation and Updates**: Automatically create and update tables based on Dataloop dataset information.
+- **Seamless Data Upload**: Upload SingleStore query results directly to Dataloop datasets.
+
+## Prerequisites
+
+To set up the integration, you'll need the following information:
+
+- **host**, which is SingleStore host url
+- **user**
+- **password**
+- **Database and Table Name** in SingleStore with at least the following columns:
+  - **`id`**: Auto-generated field.
+  - **`prompt`**: The prompt to create in Dataloop.
+  - **`response`**: Field to store model responses (auto-populated from the RLHF pipeline).
+
+## Pipeline Nodes
+
+- **Import SingleStore**
+
+  - This node retrieves prompts from a selected SingleStore table and adds them to a specified dataset in Dataloop, creating prompt items accordingly.
+
+- **Export SingleStore**
+  - This node takes the response marked as the best and updates the corresponding SingleStore table row with the response, model name and id from Dataloop.
---
+++ nodes/import_export/singlestore_base.py
@@ -0,0 +1,160 @@
+import os
+import logging
+import dtlpy as dl
+import pymysql
+from pymysql.cursors import DictCursor
+
+logger = logging.getLogger(name="singlestore-connect")
+
+
+class SingleStoreBase(dl.BaseServiceRunner):
+    """
+    A class for running a service that interacts with SingleStore.
+    """
+
+    def __init__(self):
+        """
+        Initializes the ServiceRunner with SingleStore credentials.
+        """
+        self.logger = logger
+
+    def table_to_dataloop(
+        self,
+        host: str,
+        user: str,
+        database: str,
+        table_name: str,
+        dataset_id: str,
+    ):
+        """
+        Fetches data from a SingleStore table and uploads it to a Dataloop dataset.
+
+        :param host: SingleStore host url.
+        :param user: SingleStore username.
+        :param database: SingleStore database name.
+        :param table_name: SingleStore table name.
+        :param dataset_id: Dataloop dataset ID.
+        :return: List of uploaded PromptItems.
+        """
+
+        self.logger.info(
+            "Creating table for dataset '%s' and table '%s'.", dataset_id, table_name
+        )
+
+        try:
+            dataset = dl.datasets.get(dataset_id=dataset_id)
+            self.logger.info("Successfully retrieved dataset with ID '%s'.", dataset_id)
+        except dl.exceptions.NotFound as e:
+            self.logger.error("Failed to get dataset with ID '%s': %s", dataset_id, e)
+            raise e
+
+        # Execute query to fetch data
+        query = f"SELECT * FROM {table_name}"
+
+        db_config = {
+            "host": host,
+            "port": 3306,
+            "user": user,
+            "password": os.environ.get("SINGLESTORE_PASSWORD"),
+            "database": database,
+        }
+        with pymysql.connect(**db_config) as connection:
+            with connection.cursor(DictCursor) as cursor:
+                cursor.execute(query)
+                rows = cursor.fetchall()
+
+        prompt_items = []
+        for row in rows:
+            prompt_item = dl.PromptItem(name=str(rows["id"]))
+            prompt_item.add(
+                message={
+                    "role": "user",
+                    "content": [
+                        {"mimetype": dl.PromptType.TEXT, "value": row["prompt"]}
+                    ],
+                }
+            )
+            prompt_items.append(prompt_item)
+
+        # Upload PromptItems to Dataloop
+        items = list(dataset.items.upload(local_path=prompt_items, overwrite=True))
+        self.logger.info(
+            "Successfully uploaded %d items to dataset '%s'.", len(items), dataset_id
+        )
+        return items
+
+    def update_table(
+        self,
+        item: dl.Item,
+        host: str,
+        user: str,
+        database: str,
+        table_name: str,
+    ):
+        """
+        Updates a SingleStore table with the best response from a Dataloop item.
+
+        :param item: Dataloop item.
+        :param host: SingleStore host url.
+        :param user: SingleStore username.
+        :param database: SingleStore database name.
+        :param table_name: SingleStore table name.
+        :return: The updated Dataloop item.
+        """
+
+        self.logger.info(
+            "Updating table '%s' for item with ID '%s'.", table_name, item.id
+        )
+
+        prompt_item = dl.PromptItem.from_item(item)
+        first_prompt_key = prompt_item.prompts[0].key
+
+        # Find the best response based on annotation attributes
+        best_response = None
+
+        for resp in item.annotations.list():
+            try:
+                is_best = resp.attributes.get("isBest", False)
+            except AttributeError:
+                is_best = False
+            if is_best and resp.metadata["system"].get("promptId") == first_prompt_key:
+                best_response = resp.coordinates
+                break
+
+        if best_response is None:
+            self.logger.error("No best response found for item ID: %s", item.id)
+            raise ValueError(f"No best response found for item ID: {item.id}")
+
+        db_config = {
+            "host": host,
+            "port": 3306,
+            "user": user,
+            "password": os.environ.get("SINGLESTORE_PASSWORD"),
+            "database": database,
+        }
+        with pymysql.connect(**db_config) as connection:
+            with connection.cursor() as cursor:
+                update_query = f"""
+                                UPDATE {table_name}
+                                SET RESPONSE = %s
+                                WHERE id = %s
+                                """
+                cursor.execute(
+                    update_query, (best_response, int(prompt_item.name[:-5]))
+                )
+                connection.commit()
+        self.logger.info(
+            "Updating table '%s' for item with ID '%s' with response: %s",
+            table_name,
+            item.id,
+            best_response,
+        )
+        cursor.execute(update_query, (best_response, int(prompt_item.name[:-5])))
+
+        self.logger.info(
+            "Successfully updated table '%s' for item with ID '%s'.",
+            table_name,
+            item.id,
+        )
+
+        return item
